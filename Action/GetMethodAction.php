876ce2ded895d3a2f9fbf1adc8c3dd8fa7578459
Omid Mottaghi <omidmr@gmail.com>
2015-08-02 00:22:59 +0430

    fix pages loading

--- Action/GetMethodAction.php
+++ Action/GetMethodAction.php
@@ -16,6 +16,18 @@ class GetMethodAction extends AppAction
 {
     public function __invoke($slug = '')
     {
+        TwigHelper::addCss('file:////netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css');
+        TwigHelper::addCss('file:////netdna.bootstrapcdn.com/font-awesome/4.0.0/css/font-awesome.css');
+        TwigHelper::addCss('file:///Pages/css/Pages.css', 10);
+
+        TwigHelper::addJs('file:////ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular.min.js', -1000);
+        TwigHelper::addJs('file:////ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular-resource.min.js');
+        TwigHelper::addJs('file:////ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular-route.min.js');
+        TwigHelper::addJs('file:///Pages/js/angular/Pages.js', 10);
+        TwigHelper::addJs('file:///Pages/js/angular/PagesController.js', 20);
+
+        TwigHelper::addMasterTwig('@App/master.twig');
+
         /** @var \Cake\ORM\Table $pagesTable */
         $pagesTable = TableRegistry::get('Pages.Pages');
 
@@ -34,7 +46,7 @@ class GetMethodAction extends AppAction
 
         TwigHelper::addJs('angular
             .module("Pages", [])
-            .value("URL", "' . $this->getRouter()->generateUrl('pages', [Router::GEN_OPT_WITH_PARAMS => false]) . '")
+            .value("URL", "' . $this->getRouter()->generateUrl(['pages'], [Router::GEN_OPT_WITH_PARAMS => false]) . '")
             .value("page", "' . $page['slug'] . '");', 5);
     }
 }
--- Bootstrap.php
+++ Bootstrap.php
@@ -14,16 +14,5 @@ class Bootstrap extends Bundle
 {
     public function startup()
     {
-        TwigHelper::addCss('file:////netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css');
-        TwigHelper::addCss('file:////netdna.bootstrapcdn.com/font-awesome/4.0.0/css/font-awesome.css');
-        TwigHelper::addCss('file:///Pages/css/Pages.css', 10);
-
-        TwigHelper::addJs('file:////ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular.min.js');
-        TwigHelper::addJs('file:////ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular-resource.min.js');
-        TwigHelper::addJs('file:////ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular-route.min.js');
-        TwigHelper::addJs('file:///Pages/js/angular/Pages.js', 10);
-        TwigHelper::addJs('file:///Pages/js/angular/PagesController.js', 20);
-
-        TwigHelper::addMasterTwig('@App/master.twig');
     }
 }
