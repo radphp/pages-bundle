c2f21a22c75274931a2a81a7ffbfaf692b935f1a
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-08-28 20:22:50 +0430

    Add "Initial" migration

---
+++ Resource/migrations/20150828132631_initial.php
@@ -0,0 +1,36 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+/**
+ * Initial Pages
+ */
+class Initial extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     */
+    public function change()
+    {
+        $this->execute(
+            'CREATE TABLE "pages" (
+"id" BIGSERIAL,
+"slug" VARCHAR NOT NULL,
+"title" VARCHAR,
+"body" TEXT,
+"created_at" TIMESTAMP,
+"updated_at" TIMESTAMP,
+CONSTRAINT "pages_slug_unique" UNIQUE ("slug"),
+PRIMARY KEY ("id")
+);
+CREATE INDEX "pages_created_at_index" ON "pages" ("created_at");
+CREATE INDEX "pages_updated_at_index" ON "pages" ("updated_at");
+'
+        );
+    }
+}
